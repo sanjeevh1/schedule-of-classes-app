ae2f2f7cf5b510f90444c1ac9d5a15a6161a81dd
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2024-08-16 16:30:42 -0700

    Refactored MainActivity into multiple files w. previews

--- app/src/main/java/com/example/rutgersscheduleofclasses/MainActivity.kt
+++ app/src/main/java/com/example/rutgersscheduleofclasses/MainActivity.kt
@@ -5,55 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.rutgersscheduleofclasses.data.Repository.campuses
-import com.example.rutgersscheduleofclasses.data.Repository.levels
-import com.example.rutgersscheduleofclasses.data.Repository.subjects
-import com.example.rutgersscheduleofclasses.data.Repository.terms
-import com.example.rutgersscheduleofclasses.model.Course
-import com.example.rutgersscheduleofclasses.model.sectionData.Section
+import com.example.rutgersscheduleofclasses.ui.CourseCard
 import com.example.rutgersscheduleofclasses.ui.CoursesViewModel
+import com.example.rutgersscheduleofclasses.ui.PromptCard
 import com.example.rutgersscheduleofclasses.ui.theme.RutgersScheduleOfClassesTheme
-import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,265 +36,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ScheduleOfClassesApp(modifier = Modifier.padding(innerPadding))
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-//A box displaying the information for a given section
-@Composable
-fun SectionBox(section: Section, modifier: Modifier = Modifier) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = colorResource(
-                id = if(section.openStatus) {
-                    R.color.green
-                } else {
-                    R.color.scarlet
-                }
-            ),
-            contentColor = colorResource(id = R.color.white)
-        ),
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Column(
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = section.number)
-                Text(text = section.openStatusText)
-            }
-            Text(text = "Index: ${section.index}")
-            Text(text = "Instructors: ${section.instructorsText}")
-            //add meeting times
-            Text(text = "Meeting Times:")
-            for (meetingTime in section.meetingTimes) {
-                Text(
-                    text = meetingTime.toString(),
-                    modifier = Modifier.padding(top = 5.dp)
-                )
-            }
-        }
-    }
-}
-
-//A card displaying the information for a given course
-@Composable
-fun CourseCard(course: Course, modifier: Modifier = Modifier) {
-    var showSections by remember { mutableStateOf(false) }
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = colorResource(id = R.color.gray),
-            contentColor = colorResource(id = R.color.white)
-        ),
-        modifier = modifier
-    ) {
-        Column(
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = course.courseString)
-                IconButton(onClick = { showSections = !showSections }) {
-                    Icon(
-                        imageVector = if (showSections) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                        contentDescription = null,
-                        tint = colorResource(R.color.white)
-                    )
-                }
-            }
-            Text(text = course.title)
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = course.creditsObject.description)
-                Text(text = "sections: ${course.openSections}/${course.sections.size}")
-            }
-            if(showSections) {
-                for (section in course.sections) {
-                    SectionBox(
-                        section = section,
-                        modifier = Modifier.padding(
-                            top = 10.dp
-                        )
-                    )
-                }
-            }
-
-        }
-    }
-}
-
-@Composable
-fun Prompt(
-    label: String,
-    value: String?,
-    map: Map<String,String>,
-    coursesViewModel: CoursesViewModel,
-    onResponse: (CoursesViewModel,String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    var menuExpanded by remember { mutableStateOf(false) }
-    var textFieldSize by remember { mutableStateOf(Size.Zero) }
-
-    Box(
-        modifier = modifier.background(colorResource(id = R.color.gray))
-    ) {
-        TextField(
-            value = value?: "",
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = label) },
-            trailingIcon = {
-                IconButton(
-                    onClick = { menuExpanded = !menuExpanded },
-                    content = {
-                        Icon(
-                            imageVector = if (menuExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                            contentDescription = null,
-                        )
-                    }
-                )
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(id = R.color.gray))
-                .onGloballyPositioned { coordinates ->
-                    textFieldSize = coordinates.size.toSize()
-                }
-        )
-        DropdownMenu(
-            expanded = menuExpanded,
-            onDismissRequest = { menuExpanded = false },
-            modifier = Modifier
-                .background(colorResource(id = R.color.gray))
-                .width(
-                    with(LocalDensity.current) { textFieldSize.width.toDp() }
-                )
-        ) {
-            map.forEach { entry ->
-                DropdownMenuItem(
-                    text = {
-                        Text(
-                            text = entry.value,
-                            color = colorResource(id = R.color.white)
-                        )
-                           },
-                    onClick = {
-                        coursesViewModel.hideCourses()
-                        onResponse(coursesViewModel,entry.key)
-                        menuExpanded = false
-                    },
-                )
-            }
-        }
-    }
-}
-
-//returns a map of every year as a string to itself
-fun getYearMap(): Map<String,String> {
-    val map = mutableMapOf<String,String>()
-    for (year in 2021..Calendar.getInstance().get(Calendar.YEAR) + 1) {
-        map[year.toString()] = year.toString()
-    }
-    return map
-}
-
-//A card prompting the user for input
-@Composable
-fun PromptCard(
-    coursesViewModel: CoursesViewModel,
-    modifier: Modifier = Modifier
-) {
-    val coursesUiState by coursesViewModel.uiState.collectAsState()
-
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = colorResource(id = R.color.scarlet),
-        ),
-        modifier = modifier
-    ) {
-        Column() {
-            Prompt(
-                label = "Year",
-                value = coursesUiState.year,
-                map = getYearMap(),
-                coursesViewModel = coursesViewModel,
-                onResponse = { coursesViewModel, response ->
-                    coursesViewModel.updateYear(response)
-                },
-                modifier = Modifier.padding(20.dp)
-            )
-            Prompt(
-                label = "Term",
-                value = terms[coursesUiState.term],
-                map = terms,
-                coursesViewModel = coursesViewModel,
-                onResponse = { coursesViewModel, response ->
-                    coursesViewModel.updateTerm(response)
-                },
-                modifier = Modifier.padding(20.dp)
-            )
-            Prompt(
-                label = "Campus",
-                value = campuses[coursesUiState.campus],
-                map = campuses,
-                coursesViewModel = coursesViewModel,
-                onResponse = { coursesViewModel, response ->
-                    coursesViewModel.updateCampus(response)
-                },
-                modifier = Modifier.padding(20.dp)
-            )
-            Prompt(
-                label = "Level",
-                value = levels[coursesUiState.level],
-                map = levels,
-                coursesViewModel = coursesViewModel,
-                onResponse = { coursesViewModel, response ->
-                    coursesViewModel.updateLevel(response)
-                },
-                modifier = Modifier.padding(20.dp)
-            )
-            Prompt(
-                label = "Subject",
-                value = subjects[coursesUiState.subject],
-                map = subjects,
-                coursesViewModel = coursesViewModel,
-                onResponse = { coursesViewModel, response ->
-                    coursesViewModel.updateSubject(response)
-                },
-                modifier = Modifier.padding(20.dp)
-            )
-
-            Button(
-                onClick = { coursesViewModel.setCourses() },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.gray),
-                    contentColor = colorResource(id = R.color.white)
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-            ) {
-                Text(
-                    text = "Search",
-                    color = colorResource(id = R.color.white)
-                )
             }
         }
     }
@@ -386,9 +99,10 @@ fun ScheduleOfClassesApp(
     }
 }
 
+//Preview of app
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SOCAppPreview() {
     RutgersScheduleOfClassesTheme {
         ScheduleOfClassesApp()
     }
