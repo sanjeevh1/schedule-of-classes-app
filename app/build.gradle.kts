ef7df007dbcf181248f82cccb4df5dce7282af2e
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2024-09-03 16:42:11 -0400

    Updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "my.soc.rutgersscheduleofclasses"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
