1d8256874c268bc3dcf9a8d33d79c77f0532c2d5
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2024-09-24 15:41:29 -0400

    version code updated to 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "my.soc.rutgersscheduleofclasses"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
